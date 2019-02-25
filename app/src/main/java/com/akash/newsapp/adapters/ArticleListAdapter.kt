012e4b690add94fd7b44a6ef2cb68716f13727f0
Akash <Akashkamble>
2019-02-25 21:54:16 +0530

    Improvements - Added animations in recyclerview

--- app/src/main/java/com/akash/newsapp/adapters/ArticleListAdapter.kt
+++ app/src/main/java/com/akash/newsapp/adapters/ArticleListAdapter.kt
@@ -4,6 +4,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import android.widget.TextView
 import com.akash.newsapp.R
